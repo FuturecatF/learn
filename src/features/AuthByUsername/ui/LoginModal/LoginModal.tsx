cc1911b44590a86c4087862f1bb666fe750afe90
Futurecat <futurecatforsale@gmail.com>
2023-02-22 21:09:02 +0300

    resolve lint errors v3

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -3,15 +3,13 @@ import { classNames } from 'shared/config/theme/lib/classNames';
 import { Modal } from 'shared';
 import { LoginForm } from '../../ui/LoginForm/LoginForm';
 
-import cls from './LoginModal.module.scss';
-
 interface LoginModalProps {
   className?: string;
   isOpen: boolean;
   onClose: () => void;
 }
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
-  <Modal isOpen={isOpen} onClose={onClose} className={classNames(cls.loginModal, {}, [className])} lazy>
+  <Modal isOpen={isOpen} onClose={onClose} className={classNames('', {}, [className])} lazy>
     <LoginForm />
   </Modal>
 );
